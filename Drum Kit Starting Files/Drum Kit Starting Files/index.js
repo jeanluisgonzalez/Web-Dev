91408ecc8240cebddd5808a7037ba99e044752e7
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-08-12 09:11:35 -0400

    update at work

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,3 +1,37 @@
+var tom1 = new Audio('sounds/tom-1.mp3');
+var tom2 = new Audio('sounds/tom-2.mp3');
+var tom3 = new Audio('sounds/tom-3.mp3');
+var tom4 = new Audio('sounds/tom-4.mp3');
+var crash = new Audio('sounds/crash.mp3');
+var kick = new Audio('sounds/kick-bass.mp3');
+var snare = new Audio('sounds/snare.mp3');
 document.querySelectorAll(".drum").forEach(element => element.addEventListener("click",function(){
-  alert("I got clicked! "+element.textContent);
+
+element.style.color="white";
+var key=element.textContent;
+switch(key){
+  case "w":
+    tom1.play();
+    break;
+  case "a":
+    tom2.play();
+    break;
+  case "s":
+    tom3.play();
+    break;
+  case "d":
+    tom4.play();
+    break;
+  case "j":
+    crash.play();
+    break;
+  case "k":
+    kick.play();
+    break;
+  case "l":
+    snare.play();
+    break;
+  default: console.log(key);
+}
+
 }));
