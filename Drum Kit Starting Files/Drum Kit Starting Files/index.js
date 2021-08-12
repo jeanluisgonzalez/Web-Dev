a8657023d14bd7bf39bed0188edf856f273e5e66
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-08-12 14:26:36 -0400

    Update at work

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -5,33 +5,133 @@ var tom4 = new Audio('sounds/tom-4.mp3');
 var crash = new Audio('sounds/crash.mp3');
 var kick = new Audio('sounds/kick-bass.mp3');
 var snare = new Audio('sounds/snare.mp3');
-document.querySelectorAll(".drum").forEach(element => element.addEventListener("click",function(){
-
-element.style.color="white";
-var key=element.textContent;
-switch(key){
-  case "w":
-    tom1.play();
-    break;
-  case "a":
-    tom2.play();
-    break;
-  case "s":
-    tom3.play();
-    break;
-  case "d":
-    tom4.play();
-    break;
-  case "j":
-    crash.play();
-    break;
-  case "k":
-    kick.play();
-    break;
-  case "l":
-    snare.play();
-    break;
-  default: console.log(key);
-}
-
+document.querySelectorAll(".drum").forEach(element => element.addEventListener("mousedown", function() {
+  var key = element.textContent;
+  playSounds(key);
+  buttonAnimation("add", key);
 }));
+document.querySelectorAll(".drum").forEach(element => element.addEventListener("mouseup", function() {
+  var key = element.textContent;
+  buttonAnimation("remove", key);
+}));
+
+document.addEventListener('keydown', function(event) {
+  var key = event.key;
+  playSounds(key);
+  buttonAnimation("add", key);
+
+});
+document.addEventListener('keyup', function(e) {
+  buttonAnimation("remove", event.key);
+});
+
+function playSounds(key) {
+  switch (key) {
+    case "w":
+      tom1.play();
+      break;
+    case "a":
+      tom2.play();
+      break;
+    case "s":
+      tom3.play();
+      break;
+    case "d":
+      tom4.play();
+      break;
+    case "j":
+      crash.play();
+      break;
+    case "k":
+      kick.play();
+      break;
+    case "l":
+      snare.play();
+      break;
+    default:
+      console.log(key);
+  }
+}
+  function buttonAnimation(condition, key) {
+      var activeButton = document.querySelector("." + key);
+      if (condition == "add") {
+        switch (key) {
+          case "w":
+            activeButton.classList.add("pressed");
+            break;
+          case "a":
+            activeButton.classList.add("pressed");
+            break;
+          case "s":
+            activeButton.classList.add("pressed");
+            break;
+          case "d":
+            activeButton.classList.add("pressed");
+            break;
+          case "j":
+            activeButton.classList.add("pressed");
+            break;
+          case "k":
+            activeButton.classList.add("pressed");
+            break;
+          case "l":
+            activeButton.classList.add("pressed");
+            break;
+          default:
+            console.log(key);
+        }
+      }else if(condition == "remove"){
+          switch (key) {
+            case "w":
+              activeButton.classList.remove("pressed");
+              break;
+            case "a":
+              activeButton.classList.remove("pressed");
+              break;
+            case "s":
+              activeButton.classList.remove("pressed");
+              break;
+            case "d":
+              activeButton.classList.remove("pressed");
+              break;
+            case "j":
+              activeButton.classList.remove("pressed");
+              break;
+            case "k":
+              activeButton.classList.remove("pressed");
+              break;
+            case "l":
+              activeButton.classList.remove("pressed");
+              break;
+            default:
+              console.log(key);
+          }
+        }else{
+          switch (key) {
+            case "w":
+              activeButton.classList.toggle("pressed");
+              break;
+            case "a":
+              activeButton.classList.toggle("pressed");
+              break;
+            case "s":
+              activeButton.classList.toggle("pressed");
+              break;
+            case "d":
+              activeButton.classList.toggle("pressed");
+              break;
+            case "j":
+              activeButton.classList.toggle("pressed");
+              break;
+            case "k":
+              activeButton.classList.toggle("pressed");
+              break;
+            case "l":
+              activeButton.classList.toggle("pressed");
+              break;
+            default:
+              console.log(key);
+          }
+        }
+
+      }
