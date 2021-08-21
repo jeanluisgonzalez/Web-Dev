b091eef7a3d9e953b68f0aaaae745f8da1ff155e
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-08-21 12:09:23 -0400

    update at work

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,8 +2,12 @@
 var gamePattern = [];
 var userClickedPattern =[];
 var buttonsColours = ["blue","yellow","green","red"];
+var level = 0;
+var count = 0;
 $(document).on("keydown",function(){
   nextSequence();
+  levelUpdate();
+
   // buttonsColours.forEach(function(index){
   //   $("#"+index).click(function(){
   //     playSounds(index);
@@ -16,7 +20,13 @@ $(document).on("keydown",function(){
     userClickedPattern.push(userChosenColour);
     playSounds(userChosenColour);
     animateColor(userChosenColour);
-    console.log(userClickedPattern);
+    console.log("userClickedPattern: "+userClickedPattern);
+    checkPattern();
+
+
+
+
+
   });
   $(document).off("keydown");
 });
@@ -33,11 +43,13 @@ $(document).on("keydown",function(){
 
 
 function nextSequence(){
+  level++;
+  levelUpdate();
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonsColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  console.log(gamePattern);
-  animateColor(randomChosenColour);
+  console.log("gamePattern: " + gamePattern);
+  $("." + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSounds(randomChosenColour);
 }
 function playSounds(color){
@@ -45,11 +57,34 @@ function playSounds(color){
   colorAudio.play();
 }
 function animateColor(selector){
-  $("." + selector).fadeIn(100).fadeOut(100).fadeIn(100);
-  $("." + selector).addclass("pressed");
+  $("." + selector).addClass("pressed");
   setTimeout(function(){
     $("." + selector).removeClass("pressed");
   },100);
+}
+function levelUpdate(){
+  $("#level-title").text("Level " + level);
+}
+function checkPattern(){
+  for (i = 0; i < userClickedPattern.length; i++){
+    if(gamePattern[i] === userClickedPattern[i]){
+      count++;
+      console.log("success");
+    }else {
+      count=0;
+      level=0;
+      userClickedPattern.length = 0;
+      gamePattern.length=0;
+      console.log("wrong");
+    }
+  }
+if (gamePattern.length === userClickedPattern.length){
+  userClickedPattern.length = 0;
+  count=0;
+  setTimeout(function(){
+    nextSequence();
+  },1500);
 
+}
 
 }
