a6e1ca3cd68bb8e3dc97724e41b981acfbc5eb8d
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-08-30 15:09:09 -0400

    upddate at work

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+// root
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + "/index.html");
 });
@@ -11,12 +13,20 @@ app.post('/',(req, res) => {
 
 var num1 = Number(req.body.n1);
 var num2 = Number(req.body.n2);
-
 var result = num1 + num2;
-console.log(result);
-
+//console.log(result);
 res.send("The result of the calcualtion is " + result);
-
+});
+// bmi calculator
+app.get('/bmicalculator',(req,res)=>{
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+app.post('/bmicalculator',(req,res)=>{
+  var h = Number(req.body.h);
+  var w = Number(req.body.w);
+  var bmi = w/Math.pow(h,2);
+  //console.log(bmi);
+  res.send("Your BMI is "+ bmi);
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
