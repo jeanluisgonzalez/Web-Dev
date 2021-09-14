c1f9b9b86f126b7d3fdafa53d3dd0624f839eaa0
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-09-14 16:58:54 -0400

    update at work

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-Parser");
-const request =require("request");
+const request = require("request");
 
 const app = express();
 app.use(express.static("public"));
@@ -10,16 +10,41 @@ app.get("/",(req,res)=>{
   res.sendFile(__dirname+"/signup.html");
 });
 app.post("/",(req,res)=>{
-  var fistName = req.body.fistName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-
-  var data = {
-    
+  const fistName = req.body.fistName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const list = "0d09891d17";
+  const data = {
+    members: [
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields: {
+          FNAME: fistName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  }
+  const jsonData = JSON.stringify(data);
+  const url ="https://us5.api.mailchimp.com/3.0/lists/"+list
+  const options = {
+    method: "POST",
+    auth: "jgonzaleza:b2ba8081c2f545f8c998c0f0f2f00a3f-us5"
   }
 
 
-  res.sendFile(__dirname+"/signup.html");
+
+
+  const jsonRequest =  https.request(url, options, (response)=>{
+    response.on("data",(data)=>{
+      //console.log(JSON.parse(data));
+    });
+    console.log(response);
+  });
+  jsonRequest.write(jsonData);
+  jsonRequest.end();
+  //res.sendFile(__dirname+"/signup.html");
 });
 app.listen(3000,()=>{
   console.log("Server is running on port 3000");
