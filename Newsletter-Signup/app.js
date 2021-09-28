3387e93cd60354a4d52b4541df1aa36c314c5c7b
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-09-28 11:20:22 -0400

    update at work

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const https = require("https");
-const bodyParser = require("body-Parser");
+const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
@@ -54,10 +54,9 @@ app.post("/",(req,res)=>{
 app.post("/failure",(req,res)=>{
   res.redirect("/");
 });
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
   console.log("Server is running on port 3000");
 });
-
 //API Key
 // b2ba8081c2f545f8c998c0f0f2f00a3f-us5
 
