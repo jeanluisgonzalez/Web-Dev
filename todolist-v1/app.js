6761866bd7aa84605fc37c716ba4548ee6c048b2
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-10-04 21:05:56 -0400

    update at work

---
+++ todolist-v1/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/",(req,res)=>{
+
+  var today = new Date();
+  var currentDay = today.getDay();
+
+
+  if(currentDay === 6 || currentDay === 0){
+    res.sendFile(__dirname + "/weekend.html");
+  } else{
+    res.sendFile(__dirname + "/weekday.html");
+  }
+
+});
+
+
+
+app.listen(3000,()=>{
+  console.log("Server started on port 3000");
+});
