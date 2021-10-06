3adfbcb8d1f2585ee7c62b131344962a4795a044
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-10-06 10:38:47 -0400

    update at work

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,18 +3,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.get("/",(req,res)=>{
 
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
+  switch (currentDay) {
+    case 0:
+      day="Sunday";
+      break;
+    case 1:
+      day="Monday";
+      break;
+    case 2:
+      day="Tuesday";
+      break;
+    case 3:
+      day="Wednesday";
+      break;
+    case 4:
+      day="Thursday";
+      break;
+    case 5:
+      day="Friday";
+      break;
+    case 6:
+      day="Saturday";
+      break;
 
-  if(currentDay === 6 || currentDay === 0){
-    res.sendFile(__dirname + "/weekend.html");
-  } else{
-    res.sendFile(__dirname + "/weekday.html");
+    default:
+    console.log("Error: current day is equal to:"+currentDay);
   }
-
+  res.render("list",{day:day});
 });
 
 
