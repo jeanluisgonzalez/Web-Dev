9d628165395b5eb565711098e1da1795cc0a0fe9
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-10-07 11:28:25 -0400

    Uptade at work Section 22. Video 271.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,46 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
+let items =["Buy Food"];
+let workItems = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let today = new Date();
+  let options ={
+    weekday: "long",
+    day: "numeric",
+    month:"long"
+  };
 
-  switch (currentDay) {
-    case 0:
-      day="Sunday";
-      break;
-    case 1:
-      day="Monday";
-      break;
-    case 2:
-      day="Tuesday";
-      break;
-    case 3:
-      day="Wednesday";
-      break;
-    case 4:
-      day="Thursday";
-      break;
-    case 5:
-      day="Friday";
-      break;
-    case 6:
-      day="Saturday";
-      break;
+  let day = today.toLocaleDateString("en-US",options);
 
-    default:
-    console.log("Error: current day is equal to:"+currentDay);
-  }
-  res.render("list",{day:day});
+
+  res.render("list",{listTitle:day,items: items});
 });
 
+app.post("/",(req,res)=>{
+  let item = req.body.newItem;
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
 
+});
 
+app.get("/work",(req,res)=>{
+  res.render("list",{listTitle:"Work List",items:workItems});
+});
+app.get("/about",(req,res)=>{
+  res.render("about");
+});
 app.listen(3000,()=>{
   console.log("Server started on port 3000");
 });
