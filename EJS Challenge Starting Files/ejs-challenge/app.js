e3bedb7228afd84ba21984f0b8012ea09c795bd8
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-11-08 16:53:11 -0400

    Update at work

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -9,21 +9,28 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts = {};
+const posts = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-  res.render("home",{Content:homeStartingContent});
-  console.log(posts);
+  res.render("home",{Content:homeStartingContent,posts:posts});
 });
 app.get("/about",(req,res)=>{
   res.render("about",{Content:aboutContent});
 });
 app.get("/contact",(req,res)=>{
   res.render("contact",{Content:contactContent});
+});
+app.get("/posts/:postName",(req,res)=>{
+  const requestedTitle = req.params.postName;
+  posts.forEach(function(post){
+    if(post.title === requestedTitle ){
+      console.log("Match found!");
+    }
+  });
 });
 app.get("/compose",(req,res)=>{
   res.render("compose");
