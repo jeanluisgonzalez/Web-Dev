6380dc70a1d8f0d48a1e0538f05459918a776065
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-11-22 14:38:57 -0400

    Update at work

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,51 +1,21 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require('assert');
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb://localhost:27017";
+const mongoose = require('mongoose');
 
-const dbName = 'fruitsDB';
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-const client = new MongoClient(uri);
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  raing: Number,
+  review: String
+});
 
-// client.connect(function(err){
-//   assert.equal(null,err);
-//   console.log("Connected successfully to server");
-//   const db = client.db(dbName);
-//   client.close();
-// })
+const Fruit = mongoose.model("Fruit",fruitSchema);
 
+const fruit = new Fruit ({
+  name:"Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit."
+})
 
+fruit.save();
 
-async function insertDocuments() {
-  try {
-    await client.connect();
-    const database = client.db(dbName);
-    const fruits = database.collection("fruits");
-    // create a document to insert
-    const docs = [
-      {
-        name: "Apple",
-        score: 8,
-        review: "Great fruit"
-      },
-      {
-        name: "Orange",
-        score: 6,
-        review: "Kinda sour"
-      },
-      {
-        name: "Banana",
-        score: 8,
-        review: "Great stuff!"
-      }
-    ];
-    // this option prevents additional documents from being inserted if one fails
-    const options = { ordered: true };
-
-    const result = await fruits.insertMany(docs, options);
-    console.log(`${result.insertedCount} documents were inserted`);
-  } finally {
-    await client.close();
-  }
-}
-insertDocuments().catch(console.dir);
+// insertDocuments().catch(console.dir);
