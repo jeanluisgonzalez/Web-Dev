9f5fe610ae1c62b1e687c679736241c4684af439
jeanluisgonzalez <jeanluiss26@gmail.com>
2021-11-22 16:35:13 -0400

    Update at work

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -4,18 +4,72 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
 const fruitSchema = new mongoose.Schema({
   name: String,
-  raing: Number,
+  rating: Number,
   review: String
 });
 
-const Fruit = mongoose.model("Fruit",fruitSchema);
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
 
-const fruit = new Fruit ({
+const Fruit = mongoose.model("Fruit",fruitSchema);
+const Person = mongoose.model("Person",personSchema);
+
+const apple = new Fruit ({
   name:"Apple",
   rating: 7,
   review: "Pretty solid as a fruit."
-})
+});
+const kiwi = new Fruit({
+  name: "Kiwi",
+  rating: 8,
+  review: "Pretty tasty"
+});
+const orange = new Fruit({
+  name: "Orange",
+  rating: 9,
+  review: "Pretty good "
+});
+const banana = new Fruit({
+  name: "Banana",
+  rating: 9,
+  review: "nice fruit"
+});
 
-fruit.save();
+const person = new Person ({
+  name:"Jean",
+  Age: 25
+});
+
+Fruit.find(function(err,fruits){
+
+  if(err){
+    console.log(err);
+  }else{
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+  }
+
+  mongoose.connection.close();
+});
+// Fruit.insertMany([apple,kiwi,orange,banana],function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("Successfully saved all the fruits to fruitsDB");
+//   }
+//     mongoose.connection.close();
+// })
+
+
+// person.save(function(){
+//   mongoose.connection.close();
+// });
+
+
+// mongoose.connection.close();
+// fruit.save();
 
 // insertDocuments().catch(console.dir);
