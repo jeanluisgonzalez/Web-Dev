ccd01f7c0fd03eab3612e077ba44a1733e72dada
Jean Luis <jeanluiss26@gmail.com>
2022-01-11 22:57:55 -0400

    Navbar

--- E-Commerce/src/components/Navbar.jsx
+++ E-Commerce/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 
 const Container = styled.div`
     height: 60px;
     background-color: white;
 `;
-const SearchContainer = styled.div`
 
-`;
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
@@ -17,31 +18,67 @@ const Wrapper = styled.div`
 
 const Left= styled.div`
     flex:1;
+    display: flex;
+    align-items: center;
 `;
-
 const Language =styled.span`
     font-size: 14px;
     cursor: pointer;
 `;
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+const Input = styled.input`
+    border: none;
+`;
+
 const Center= styled.div`
     flex:1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
 `;
 const Right= styled.div`
     flex:1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-Left: 25px;
 `;
 const Navbar = () => {
     return (
         <Container>
           <Wrapper>
-              <Left>
-                  <Language>EN</Language>
-                  <SearchContainer>
-                      input
-                      <SearchIcon/>
-                  </SearchContainer>
-              </Left>
-              <Center>center</Center>
-              <Right>right</Right>
+            <Left>
+                <Language>
+                    EN
+                </Language>
+                <SearchContainer>
+                    <Input/>
+                    <SearchIcon style={{ color: "gray", fontSize:16}}/>
+                </SearchContainer>
+                
+            </Left>
+            <Center><Logo>OHARAJYY</Logo></Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color= "primary">
+                        <ShoppingCartOutlinedIcon/>
+                    </Badge>
+                </MenuItem>
+            </Right>
           </Wrapper>  
         </Container>
     )
