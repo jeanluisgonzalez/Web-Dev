ce195c1015940ad7ad355b4f65cec5c9d38449b5
jeanluisgonzalez <jeanluiss26@gmail.com>
2022-01-18 16:56:39 -0400

    models and routes

---
+++ E-Commerce/ECommerceAPI/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server has started successfully");
+});
