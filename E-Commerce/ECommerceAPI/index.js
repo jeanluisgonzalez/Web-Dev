55e4ce0b28571990a31a1060131607afab7897e2
jeanluisgonzalez <jeanluiss26@gmail.com>
2022-01-28 16:54:21 -0400

    Cart CRUD

--- E-Commerce/ECommerceAPI/index.js
+++ E-Commerce/ECommerceAPI/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 const app = express();
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server has started successfully");
--- E-Commerce/ECommerceAPI/routes/product.js
+++ E-Commerce/ECommerceAPI/routes/product.js
@@ -70,40 +70,19 @@ router.get("/", verifyTokenAndAuthorization, async (req, res) => {
     if (qNew) {
       products = await Product.find().sort({ createdAt: -1 }).limit(5);
     } else if (qCategory) {
-      products = await Product;
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
     }
 
-    res.status(200).json(product);
+    res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-// //GET USER STATS
-
-// router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-//   const date = new Date();
-//   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-//   try {
-//     const data = await User.aggregate([
-//       { $match: { createdAt: { $gte: lastYear } } },
-//       {
-//         $project: {
-//           month: { $month: "$createdAt" },
-//         },
-//       },
-//       {
-//         $group: {
-//           _id: "$month",
-//           total: { $sum: 1 },
-//         },
-//       },
-//     ]);
-//     res.status(200).json(data);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
