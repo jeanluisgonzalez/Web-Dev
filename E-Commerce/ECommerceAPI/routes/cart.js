0a0bee982c1537024681accc4222982d72cad578
jeanluisgonzalez <jeanluiss26@gmail.com>
2022-02-05 08:54:46 -0400

    cart and order CRUD

--- E-Commerce/ECommerceAPI/routes/cart.js
+++ E-Commerce/ECommerceAPI/routes/cart.js
@@ -52,8 +52,8 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
 router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const cartFound = await Cart.find({ userId: req.params.userId });
-    res.status(200).json(cartFound);
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -61,6 +61,13 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
 
 //GET ALL
 
-router.get("/", ve);
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
