3abd40f4761b5613b5cbeea8c38e9c930620bfa6
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2022-10-07 16:51:57 -0400

    update at work

--- react-blog/src/components/singlePost/SinglePost.jsx
+++ react-blog/src/components/singlePost/SinglePost.jsx
@@ -10,12 +10,40 @@ export default function SinglePost() {
           alt=""
           className="singlePostImg"
         />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet
+          <div className="singlePostEdit">
+            <i className="SinglePostIcon fa-regular fa-pen-to-square"></i>
+            <i className="SinglePostIcon fa-regular fa-trash-can"></i>
+          </div>
+        </h1>
+        <div className="siglePostInfo">
+          <span className="SinglePostAuthor">
+            Author: <b>Jean </b>
+          </span>
+          <span className="SinglePostDate">
+            Date: <b>1 hour ago </b>
+          </span>
+          <span className="SinglePostDate">
+            Date: <b>1 hour ago</b>
+          </span>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+          accusamus, ratione suscipit sequi sit cum veniam nulla. Dicta
+          perferendis consequuntur at rem ipsum minus sequi, vero odit, nulla
+          voluptatibus excepturi? Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Nobis accusamus, ratione suscipit sequi sit cum
+          veniam nulla. Dicta perferendis consequuntur at rem ipsum minus sequi,
+          vero odit, nulla voluptatibus excepturi? Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Nobis accusamus, ratione suscipit sequi
+          sit cum veniam nulla. Dicta perferendis consequuntur at rem ipsum
+          minus sequi, vero odit, nulla voluptatibus excepturi? Lorem ipsum,
+          dolor sit amet consectetur adipisicing elit. Nobis accusamus, ratione
+          suscipit sequi sit cum veniam nulla. Dicta perferendis consequuntur at
+          rem ipsum minus sequi, vero odit, nulla voluptatibus excepturi?
+        </p>
       </div>
-      <h1 className="singlePostTitle">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum eveniet
-        dolor eum aliquam fuga optio culpa architecto ab consectetur error amet
-        sint soluta suscipit, quisquam alias voluptates atque libero odio!
-      </h1>
     </div>
   );
 }
