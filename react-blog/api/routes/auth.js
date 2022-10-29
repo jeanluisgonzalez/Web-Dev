6b575a4d7394c3dcb37b0032b9a0eee895977ea0
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2022-10-29 10:12:59 -0400

    update

--- react-blog/api/routes/auth.js
+++ react-blog/api/routes/auth.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //REGISTER
 
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
 
     const user = await newUser.save();
@@ -17,3 +20,22 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(400).json("Wrong credentials!");
+    } else {
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      const { password, ...others } = user._doc;
+      !validated
+        ? res.status(400).json("Wrong credentials!")
+        : res.status(200).json(others);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
