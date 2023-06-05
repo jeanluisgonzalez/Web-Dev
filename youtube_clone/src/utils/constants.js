f215689ae64a8f6197953998515cbccf62c0751b
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-06-05 10:24:36 -0400

    update at work

--- youtube_clone/src/utils/constants.js
+++ youtube_clone/src/utils/constants.js
@@ -16,6 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
+  { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Sport', icon: <FitnessCenterIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
--- youtube_clone/src/utils/fetchFromAPI.js
+++ youtube_clone/src/utils/fetchFromAPI.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 const options = {
-    url: BASE_URL,
     params: {
       maxResults: '50'
     },
@@ -11,3 +10,9 @@ const options = {
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
+
+  export const fetchFromAPI = async(url) => {
+    const {data}=await axios.get(`${BASE_URL}/${url}`,options);
+
+    return data;
+  }
