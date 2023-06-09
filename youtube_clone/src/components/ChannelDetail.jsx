94006827540e257c43796552a9ceea8def7e4a8e
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-06-09 10:41:43 -0400

    update at work

--- youtube_clone/src/components/ChannelDetail.jsx
+++ youtube_clone/src/components/ChannelDetail.jsx
@@ -4,6 +4,7 @@ import { Box } from "@mui/material";
 
 import {Videos, ChannelCard} from './';
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Gradient } from "@mui/icons-material";
 
 const ChannelDetail = () => {
   const [channelDetail, setchannelDetail] = useState(null);
@@ -20,7 +21,21 @@ const ChannelDetail = () => {
   },[id]);
 
   return(
-    <div> {id} </div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-Gradient(90deg, rgba(0,238,247,1)0%, rgba(206,3,184,1)100%,rgba(0,212,255,1)100%)',
+          zIndex: 10,
+          height: '300px'
+        }}/>
+          <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+      </Box>
+      <Box display='flex' p="2">
+        <Box sx={{ mr:{sm:'100px'}}}/>
+          <Videos videos={videos}/>
+        
+      </Box>
+    </Box>
   )
 };
 
