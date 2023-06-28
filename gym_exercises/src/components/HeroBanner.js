52b76a73f080233b7bc8057c428cdb5410689ced
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-06-28 09:31:49 -0400

    update at work

---
+++ gym_exercises/src/components/HeroBanner.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Box, Stack, Typography} from '@mui/material'
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{
+        mt: {lg:'212px',xs: '70px'},
+        ml: {sm:'50px'}
+    }}
+    position="relative" 
+    p="px"
+    >
+        <Typography 
+        color="#FF2625"
+        fontWeight="600"
+        fontSize="26px"
+        >
+            Fitness Club
+        </Typography>
+        <Typography
+        fontWeight="700"
+        sx={{
+            fontSize:{lg:'44px', xs:'40px'}
+        
+        }}
+        >
+            Swet, Smile <br /> and Repeat
+        </Typography>
+        <Typography
+        fontSize="22px"
+        >
+            Check out the most effective exercises
+        </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
