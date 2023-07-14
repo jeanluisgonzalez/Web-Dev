064f4861ddd1188775c1fde76560b636a1c008b1
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-07-14 16:59:01 -0400

    update at work

--- 3d_Portfolio/src/components/About.jsx
+++ 3d_Portfolio/src/components/About.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12
+          min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {" "}
+            {title}{" "}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a web developer with a strong skill set in JavaScript, React,
+        Node.js, MongoDB, and MySQL. I specialize in building dynamic and
+        engaging user experiences, utilizing my solid understanding of these
+        technologies. <br /> <br />
+        With JavaScript as my primary language, I create efficient frontend
+        functionality and implement advanced interactive features. I have
+        experience with React, allowing me to design reusable and scalable user
+        interfaces that enhance performance and deliver a smooth user
+        experience. <br /> <br />
+        On the backend, I excel in developing server-side applications and APIs
+        using Node.js and Express.js. I ensure secure authentication, handle
+        data validation, and integrate databases like MongoDB and MySQL to build
+        robust and reliable backend solutions. <br /> <br />
+        My database expertise extends to both NoSQL (MongoDB) and relational
+        databases (MySQL), enabling me to design efficient data models and write
+        optimized queries. I seamlessly integrate databases with the backend
+        stack to meet specific application requirements. <br /> <br />
+        Passionate about continuous learning and professional growth, I stay
+        updated with the latest industry trends and best practices.
+        Collaborating effectively with cross-functional teams, I deliver
+        high-quality solutions that meet business requirements. <br /> <br />I
+        am excited to leverage my skills in JavaScript, React, Node.js, MongoDB,
+        and MySQL to build scalable, performant, and user-friendly web
+        applications.
+      </motion.p>
 
-export default About
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((services, index) => (
+          <ServiceCard key={services.title} index={index} {...services} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
