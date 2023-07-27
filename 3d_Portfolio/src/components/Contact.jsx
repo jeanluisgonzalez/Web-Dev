7823d3817f3810fffb0138481d8d40f93c1942df
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-07-27 15:14:55 -0400

    update: finish portfolio

--- 3d_Portfolio/src/components/Contact.jsx
+++ 3d_Portfolio/src/components/Contact.jsx
@@ -7,6 +7,9 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//
+//
+//
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +19,48 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_ydufo49",
+        "template_ap82wyj",
+        {
+          from_name: form.name,
+          to_name: "Jean L Gonzalez",
+          from_email: form.email,
+          to_email: "jeanluiss26@gmail.com",
+          message: form.message,
+        },
+        "ZOjrErdjrV0GbRDhS"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+
+          alert("Somthing went wrong.");
+        }
+      );
+  };
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
@@ -66,8 +108,19 @@ const Contact = () => {
             />
           </label>
 
-          <button type="submit">{loading ? "Seding..." : "Send"}</button>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? "Seding..." : "Send"}
+          </button>
         </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   );
